1cc97caef7d4006779df011ff8793fbbfe933a11
aslered <yurazzzzzzzzzzz6@gmail.com>
2024-12-07 09:45:07 +0000

    dfgdg

--- lab_5/task_4.py
+++ lab_5/task_4.py
@@ -2,6 +2,6 @@ import random
 
 flowers = ["Роза", "Тюльпан", "Мак", "Подсолнух"]
 colors = ["красный", "желтый", "белый", "синий", "фиолетовый", "оранжевый"]
-flower_colors = dict(zip(flowers, random.choices(colors, k=len(flowers))))
+flower_colors = dict(zip(flowers, random.randint(colors, k=len(flowers))))
 
 print(flower_colors)
